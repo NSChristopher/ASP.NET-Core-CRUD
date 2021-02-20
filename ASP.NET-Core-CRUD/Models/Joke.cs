ce2c1580f4c25ad4c0bfd5e46d4bc7d9f720f8c2
NSChristopher <noah.s.cps@gmail.com>
2021-02-20 11:27:12 -0500

    added jokes model

---
+++ ASP.NET-Core-CRUD/Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_CRUD.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswere { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
